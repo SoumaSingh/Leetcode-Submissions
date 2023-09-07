13fd430e441abe21cba77fc0fb278776be0c1e71
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2023-09-07 20:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (12.22%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
+public:
+    ListNode* reverseBetween(ListNode *head, int m, int n) {
+    ListNode dummy(0), *prev = &dummy;
+    dummy.next = head;
+    for (int i=1; i<m; i++)
+        prev = prev->next;
+    ListNode *pivot = prev->next;
+    for (int i=m; i<n; i++) {
+        swap(prev->next, pivot->next->next);
+        swap(prev->next, pivot->next);
+    }
+    return dummy.next;
+}
+};
