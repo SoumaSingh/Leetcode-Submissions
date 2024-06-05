262eca15d66c9fa982eb7fec97ea04bb21f0e58c
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-06-06 01:49:40 +0530

    Time: 36 ms (97.84%) | Memory: 42.7 MB (71.75%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    int bs(vector <int>& A, int target){
+
+        int l = 0;
+        int p = A.size()-1;
+        int s;
+        while(l<p){
+            s = (l+p)/2;
+            if(A[s] < target){
+                l = s+1;
+            }else{
+                p=s;
+            }
+        }
+        return l;
+    }
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        
+        int sumA = 0;
+        int sumB = 0;
+        for(int i = 0; i < aliceSizes.size(); i++){
+            sumA += aliceSizes[i];
+        }
+        for(int i = 0; i < bobSizes.size(); i++){
+            sumB += bobSizes[i];
+        }
+        int diff = sumA-sumB;
+        vector<int> res;
+        sort(bobSizes.begin(), bobSizes.end());
+        for(int i = 0; i<aliceSizes.size(); i++){
+            int bob = bs(bobSizes, aliceSizes[i]-(diff)/2);
+            if(bobSizes[bob] ==  (aliceSizes[i]-(diff)/2)){
+                res.push_back(aliceSizes[i]);
+                res.push_back(bobSizes[bob]);
+                break;
+            }
+        }
+        return res;
+    }
+};
