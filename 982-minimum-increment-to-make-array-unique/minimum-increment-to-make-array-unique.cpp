6574c638505f68357f12cb89237d2118f0e1f6d0
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-06-14 23:55:33 +0530

    Time: 89 ms (94.29%) | Memory: 68.8 MB (97.24%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
     int minIncrementForUnique(vector<int>& nums) {
         int moves=0;
         sort(nums.begin(),nums.end());
