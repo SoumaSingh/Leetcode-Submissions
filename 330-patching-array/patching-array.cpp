e7c45bb2faf66c71da80b6b5af98f975802beaee
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-06-16 23:24:27 +0530

    Time: 9 ms (21.31%) | Memory: 13.8 MB (45.41%) - LeetSync

--- 330-patching-array/patching-array.cpp
+++ 330-patching-array/patching-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
     int minPatches(vector<int>& nums, int n) {
         long miss=1, add=0, i=0;
         while(miss<=n)
