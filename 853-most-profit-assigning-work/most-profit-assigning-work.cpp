bb59dfa3d32e751a9d85091cad9ef5b9b37d2e7d
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-06-19 00:44:01 +0530

    Time: 37 ms (95.10%) | Memory: 41.1 MB (66.37%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         vector<pair<int,int>>profile;
         profile.push_back({0,0});
