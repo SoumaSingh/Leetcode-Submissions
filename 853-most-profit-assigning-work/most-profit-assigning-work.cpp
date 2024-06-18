8a6885620e53f21446e49ed55197196d3e08971e
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-06-19 00:43:39 +0530

    Time: 68 ms (34.66%) | Memory: 41.2 MB (47.29%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int,int>>profile;
+        profile.push_back({0,0});
+        for(int i=0;i<difficulty.size();i++)
+        {
+            profile.push_back({difficulty[i],profit[i]});
+        }
+        sort(profile.begin(),profile.end());
+        for(int i=0;i<profile.size()-1;i++)
+        {
+            profile[i+1].second=max(profile[i].second,profile[i+1].second);
+        }
+        int netprofit=0;
+        for(int i=0;i<worker.size();i++)
+        {
+            int ability=worker[i];
+            int l=0,r=profile.size()-1, jobprofit=0;
+            while(l<=r)
+            {
+                int mid=l+(r-l)/2;
+                if(profile[mid].first<=ability)
+                {
+                    jobprofit=max(jobprofit,profile[mid].second);
+                    l=mid+1;
+                }
+                else
+                {
+                    r=mid-1;
+                }
+            }
+            netprofit+=jobprofit;
+        }
+        return netprofit;
+    }
+};
