3df9a40a1e7c4e6ee424d67273c66769fe9c71e2
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-07-05 22:22:00 +0530

    Time: 152 ms (91.93%) | Memory: 115.6 MB (99.71%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> result = {-1, -1};
+        int minDistance = INT_MAX;
+        ListNode* previousNode = head;
+        ListNode* currentNode = head->next;
+        int currentIndex = 1;
+        int previousCriticalIndex = 0;
+        int firstCriticalIndex = 0;
+
+        while (currentNode->next != nullptr) {
+            if ((currentNode->val < previousNode->val &&
+                 currentNode->val < currentNode->next->val) ||
+                (currentNode->val > previousNode->val &&
+                 currentNode->val > currentNode->next->val)) {
+                if (previousCriticalIndex == 0) {
+                    previousCriticalIndex = currentIndex;
+                    firstCriticalIndex = currentIndex;
+                } else {
+                    minDistance =
+                        min(minDistance, currentIndex - previousCriticalIndex);
+                    previousCriticalIndex = currentIndex;
+                }
+            }
+            currentIndex++;
+            previousNode = currentNode;
+            currentNode = currentNode->next;
+        }
+        if (minDistance != INT_MAX) {
+            int maxDistance = previousCriticalIndex - firstCriticalIndex;
+            result = {minDistance, maxDistance};
+        }
+
+        return result;
+    }
+};
