6bf3d32f79c281ff205e105df16c7502166ef842
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-07-06 22:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
     int passThePillow(int n, int time) {
         int fullRounds = time / (n - 1);
         int extraTime = time % (n - 1);
