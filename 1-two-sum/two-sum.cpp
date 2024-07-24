8e10865c5106c5e683e55d3498ce29139cb5400d
SoumaSingh <143951023+SoumaSingh@users.noreply.github.com>
2024-07-25 01:08:49 +0530

    Time: 13 ms (53.79%) | Memory: 16.1 MB (5.77%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]=i;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]] = i;
         }
 
-        for(int i=0;i<nums.size();i++){
-            int res=target-nums[i];
-            if(mp.find(res)!=mp.end() && mp[res]!=i){
+        for (int i = 0; i < nums.size(); i++) {
+            int res = target - nums[i];
+            if (mp.find(res) != mp.end() && mp[res] != i) {
                 return {i, mp[res]};
             }
         }
